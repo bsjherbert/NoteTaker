b93e00fe4c89ad69403cbcc22ca4f50acf176586
Steven <bsjherbert@gmail.com>
2019-11-20 22:01:33 -0500

    updated script

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var path = require("path");
-var PORT = process.env.PORT || 6000;
-var fs = require("fs");
-var app = express()
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 6000;
+const fs = require("fs");
+const app = express();
+const notes = require("./db/db.json");
 
 app.use(express.urlencoded({
     extended: true
@@ -11,10 +12,21 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "notes.html"));
   });
 
-  app.get("*", function (req, res) {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
   });
+
+app.get("/api/notes", (req, res) => res.json(notes));
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = notes.length;
+  notes.push(newNote);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), ()=> {
+    res.json(newNote)
+  });
+});

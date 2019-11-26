b401374635e8ff4c92e03d4f7aadcd1a49d75bdc
bsjherbert <56240025+bsjherbert@users.noreply.github.com>
2019-11-26 09:42:20 -0500

    updating readme and others

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 const app = express();
 const notes = require("./db/db.json");
 
-app.use(express.urlencoded({
-  extended: true
-}));
-
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
+//GET route
 app.get("/api/notes", (req, res) => res.json(notes));
 
+//POST route
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   newNote.id = notes.length;
@@ -31,6 +30,13 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
+//DELETE route
+app.delete("/api/notes/:id", (req, res) => {
+  //this is where we will pick up 
+});
+
+
+//Listener
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
